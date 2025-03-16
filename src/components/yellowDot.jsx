28c8b1cfff2c7096d3642bee28ec3faff28aab9a
kjell-arv <kb.2028.la@gmail.com>
2025-03-16 17:23:08 +0100

    Updated yellowDot.jsx

--- src/components/yellowDot.jsx
+++ src/components/yellowDot.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import './YellowDot.css';
+import './yellowDot.css';
 import useWindowHeight from './useWindowHeight';
 
 const YellowDot = () => {
